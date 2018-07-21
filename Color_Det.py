21d27625a199eb4adcc779f6d444be260f6d3d9e
leonkt <38256492+leonkt@users.noreply.github.com>
2018-07-20 17:14:18 -0700

    contour detection added to indicate boundaries

--- Color_Det.py
+++ Color_Det.py
@@ -10,7 +10,17 @@ while True:
 	returned, img = cam.read()
 	img = cv2.resize(img, (340, 220))
 	imgV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-	
+	mask = cv2.inRange(imgV, lower, upper)
+	kernelO = np.ones((5,5))
+	kernelC = np.ones((20,20))
+
+	maskOpen = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernelO)
+	maskClose = cv2.morphologyEx(maskOpen, cv2.MORPH_CLOSE, kernelC)
+
+	i2, contours, h = cv.findContours(maskClose.copy(), cv.CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE)
+	cv2.drawContours(img, contours, -1, (255,0,0), 3)
+	for i in range(len(contours)):
+		x,y,w,h = cv2.boundingRect(conts[i])
 
 	cv2.imshow("Camera", img)
 	cv2.imshow("Masked General", mask)
